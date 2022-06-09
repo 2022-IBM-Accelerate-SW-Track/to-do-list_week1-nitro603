ff80ba865828d47f316953dd4f84d06ad4b6aaed
nitro603 <tavaracristian93@gmail.com>
2022-06-08 20:15:14 -0400

    environment setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,15 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <h1>About Me</h1> 
+        </div>
+        <div class = "left">
+          <p style = "bg-green">Hello I am here too</p>
+        </div>
+        <div class = "brief_description">
+          <p>hi deer</p>
+        </div>
       </div>
     )
   }

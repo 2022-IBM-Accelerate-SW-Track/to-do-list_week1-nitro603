8cc7bac3351718c81f5237f46a10e1a1595c0a3d
nitro603 <tavaracristian93@gmail.com>
2022-06-12 17:56:01 -0400

    slight wording edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,8 +20,8 @@ export default class About extends Component {
           <div className="name_title">Cristian Tavara</div>
           <div className="brief_description">
             <h6>I'm a Computer Science major at Brooklyn College</h6>
-            <p>
-            I love gaining new experiences, wheter it be learning about web development or learning a new submission from top position. I practice brazilian jiu jitsu and compassion. 🤠</p>
+            <p>I love gaining new experiences, wheter it be learning about web development or learning a new submission from side control.
+               I practice brazilian jiu jitsu and compassion. 🤠</p>
           </div>
         </div>
       </div>
